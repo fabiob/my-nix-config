fb94636de0f137eb6e8a4dee4ffba5b97a3a6d66
Fábio Batista <fabio@atelie.dev.br>
2025-07-10 03:37:52 -0300

    A little cleanup

--- home-manager.nix
+++ home-manager.nix
@@ -1,21 +1,14 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
+# Configuration related to the existing users and the home-manager configuration.
 
 { config, pkgs, ... }:
-
 {
   imports = [ <home-manager/nixos> ];
 
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.fabio = {
     isNormalUser = true;
     description = "Fábio Batista";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
-    #  thunderbird
     ];
   };
 
