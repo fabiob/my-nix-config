b7605bf207dce8539e4e93ebb96a021739eeda98
Fábio Batista <fabio@atelie.dev.br>
2025-08-17 09:48:03 -0300

    Calls flameshot when pressing PrtSc

--- home-manager.nix
+++ home-manager.nix
@@ -27,6 +27,7 @@
         package = pkgs.nerd-fonts.adwaita-mono;
         size = 11;
       };
+      flameshot-gui = pkgs.writeShellScriptBin "flameshot-gui" "${pkgs.flameshot}/bin/flameshot gui";
     in
     {
       home.packages = [ defaultMonoFont.package ];
@@ -36,6 +37,23 @@
       dconf.settings = {
         "org/gnome/desktop/interface" = {
           monospace-font-name = "${defaultMonoFont.name} ${toString defaultMonoFont.size}";
+        };
+
+        # Disables the default screenshot interface
+        "org/gnome/shell/keybindings" = {
+          show-screenshot-ui = [ ];
+        };
+        # Sets the new keybindings
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          custom-keybindings = [
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          ];
+        };
+        # Defines the new shortcut
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          binding = "Print";
+          command = "${flameshot-gui}/bin/flameshot-gui";
+          name = "Flameshot";
         };
       };
 
