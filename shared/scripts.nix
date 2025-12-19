ae78dcfabd6ee5b27abeda054e1098a6e8ca87dd
Fabio Batista <fabio@atelie.dev.br>
2025-12-19 19:19:59 -0300

    Updates do-nixos-upgrade command

--- shared/scripts.nix
+++ shared/scripts.nix
@@ -11,7 +11,7 @@ let
       set -exo pipefail
       cd /etc/nixos
       nix flake update
-      ${pkgs.nixos-rebuild-ng}/bin/nixos-rebuild-ng switch --flake ".#$(hostname)" -v --log-format internal-json |& ${pkgs.nix-output-monitor}/bin/nom --json
+      ${pkgs.nixos-rebuild-ng}/bin/nixos-rebuild switch --flake ".#$(hostname)" -v --log-format internal-json |& ${pkgs.nix-output-monitor}/bin/nom --json
     '';
   };
 in
