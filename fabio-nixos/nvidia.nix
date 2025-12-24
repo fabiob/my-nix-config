7bddea446a704a5ed943f9fda29bb72c7128991e
Fábio Batista <fabio@atelie.dev.br>
2025-12-23 23:14:49 -0300

    Remove nvidia patch

--- fabio-nixos/nvidia.nix
+++ fabio-nixos/nvidia.nix
@@ -50,17 +50,5 @@
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
-
-    package = config.boot.kernelPackages.nvidiaPackages.stable // {
-      open = config.boot.kernelPackages.nvidiaPackages.stable.open.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [
-          (pkgs.fetchpatch {
-            name = "get_dev_pagemap.patch";
-            url = "https://github.com/NVIDIA/open-gpu-kernel-modules/commit/3e230516034d29e84ca023fe95e284af5cd5a065.patch";
-            hash = "sha256-BhL4mtuY5W+eLofwhHVnZnVf0msDj7XBxskZi8e6/k8=";
-          })
-        ];
-      });
-    };
   };
 }
