97469d355d9fb41c1721c62d25e9dce5dae13cdf
Fábio Batista <fabio@atelie.dev.br>
2026-01-07 17:09:17 -0300

    Adds a script to update jetbrains jdbc drivers

--- shared/scripts.nix
+++ shared/scripts.nix
@@ -1,22 +1,33 @@
 { config, pkgs, ... }:
 
-let
-  do-nixos-upgrade = pkgs.writeShellApplication {
-    name = "do-nixos-upgrade";
-    runtimeInputs = [
-      pkgs.nix-output-monitor
-      pkgs.nixos-rebuild-ng
-    ];
-    text = ''
-      set -exo pipefail
-      cd /etc/nixos
-      nix flake update
-      ${pkgs.nixos-rebuild-ng}/bin/nixos-rebuild switch --flake ".#$(hostname)" -v --log-format internal-json |& ${pkgs.nix-output-monitor}/bin/nom --json
-    '';
-  };
-in
 {
+
   environment.systemPackages = [
-    do-nixos-upgrade
+    (pkgs.writeShellApplication {
+      name = "do-nixos-upgrade";
+      runtimeInputs = [
+        pkgs.nix-output-monitor
+        pkgs.nixos-rebuild-ng
+      ];
+      text = ''
+        set -exo pipefail
+        cd /etc/nixos
+        nix flake update
+        ${pkgs.nixos-rebuild-ng}/bin/nixos-rebuild switch --flake ".#$(hostname)" -v --log-format internal-json |& ${pkgs.nix-output-monitor}/bin/nom --json
+      '';
+    })
+
+    (
+      let
+        python = (pkgs.python314.withPackages (python-pkgs: [ python-pkgs.lxml ]));
+      in
+      pkgs.writeShellApplication {
+        name = "do-update-jetbrains-jdbc";
+        runtimeInputs = [ python ];
+        text = ''
+          ${python}/bin/python ${./scripts/jetbrains-update-jdbc.py}
+        '';
+      }
+    )
   ];
 }
